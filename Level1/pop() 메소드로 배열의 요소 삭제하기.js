8e801ba6d616e3e79cde80a29921cd64fcedf096
LeeDongGuk <39736916+leedongguk@users.noreply.github.com>
2022-09-02 23:42:58 +0900

    Create pop() 메소드로 배열의 요소 삭제하기.js

---
+++ Level1/pop() 메소드로 배열의 요소 삭제하기.js
@@ -0,0 +1,9 @@
+    <script>
+        var animals = ["사자", "호랑이", "사슴", "펭귄"];
+        animals.pop();
+        document.write(animals + "<br>");
+
+        var x = animals.pop();
+        document.write(x + "<br>");
+        document.write(animals);
+    </script>
