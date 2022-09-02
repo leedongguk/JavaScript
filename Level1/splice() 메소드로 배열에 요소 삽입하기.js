35de87adc6124c874c5b5b61da2bcf33c2ae4c35
LeeDongGuk <39736916+leedongguk@users.noreply.github.com>
2022-09-02 23:52:08 +0900

    Create splice() 메소드로 배열에 요소 삽입하기.js

---
+++ Level1/splice() 메소드로 배열에 요소 삽입하기.js
@@ -0,0 +1,10 @@
+    <script>
+
+        var animals = ["사자", "호랑이", "사슴", "펭귄"];
+        animals.splice(2, 2, "곰");
+        document.write(animals + "<br>");
+
+        animals.splice(2, 0, "원숭이");
+        document.write(animals);
+
+    </script>
