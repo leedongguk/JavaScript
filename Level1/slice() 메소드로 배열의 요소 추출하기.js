a35d53ccc890a9dede51055cb07e72bb2ef0b63d
LeeDongGuk <39736916+leedongguk@users.noreply.github.com>
2022-09-02 23:57:32 +0900

    Create slice() 메소드로 배열의 요소 추출하기.js

---
+++ Level1/slice() 메소드로 배열의 요소 추출하기.js
@@ -0,0 +1,12 @@
+    <script>
+
+        var animals = ["사자", "호랑이", "사슴", "펭귄", "여우", "독수리"];
+        var arr;
+
+        arr = animals.slice(2);
+        document.write(arr + "<br>");
+
+        arr = animals.slice(2, 4);
+        document.write(arr);
+
+    </script>
