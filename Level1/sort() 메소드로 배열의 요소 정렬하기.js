c7c4140b03bd3ee7a5e9d993ba2b51d4024ef1c2
LeeDongGuk <39736916+leedongguk@users.noreply.github.com>
2022-09-03 00:14:39 +0900

    Create sort() 메소드로 배열의 요소 정렬하기.js

---
+++ Level1/sort() 메소드로 배열의 요소 정렬하기.js
@@ -0,0 +1,11 @@
+    <script>
+
+        var animals = ["사자", "호랑이", "얼룩말", "펭귄"];
+
+        animals.sort();
+        document.write(animals + "<br>");
+
+        animals.reverse();
+        document.write(animals);
+
+    </script>
